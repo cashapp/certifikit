0cab15df40369bc147514277dd74b378c9add445
Yuri Schimke <yuri@schimke.ee>
2020-08-11 21:53:23 +0100

    Graal build

--- certifikit-cli/build-native.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-../gradlew clean assemble
-mkdir build/graal
-/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents/Home/bin/native-image -jar ./build/install/certifikit-cli-shadow/lib/certifikit-cli-*-all.jar --no-fallback -H:ReflectionConfigurationFiles=./build/resources/main/META-INF/native-image/app.cash.certifikit/certifikit-cli/reflect-config.json --enable-https build/graal/cft
